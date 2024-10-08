a703b2e6cb5536adcf3aa0ed0288b33948833e3d
Zuhayer Siddique <zuhayersiddique162@gmail.com>
2024-10-09 00:17:31 +0600

    Added proxy for hosting

--- app/api/proxy/route.js
+++ app/api/proxy/route.js
@@ -1,25 +1,27 @@
 // app/api/proxy/route.js
 
 export async function GET(request) {
-  // Extract URL parameters from the request
   const url = new URL(request.url);
   const entity = url.searchParams.get("entity");
   const login = url.searchParams.get("login");
   const password = url.searchParams.get("password");
 
-  // Construct the API URL with the received parameters
+  // Use HTTP in the fetch, since we're not accessing the original API via HTTPS
   const apiUrl = `http://web.fc.utm.my/ttms/web_man_webservice_json.cgi?entity=${entity}&login=${login}&password=${password}`;
 
   try {
-    const response = await fetch(apiUrl);
+    const response = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        "Accept": "application/json",
+      },
+    });
 
-    // Check if the response is OK (status in the range 200-299)
     if (!response.ok) {
       return new Response("Error fetching data", { status: response.status });
     }
 
     const data = await response.json();
-
     return new Response(JSON.stringify(data), {
       status: 200,
       headers: {
