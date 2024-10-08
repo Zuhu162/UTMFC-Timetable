9bd236d415b616d428e285b01058a0bdf1f047ea
Zuhayer Siddique <zuhayersiddique162@gmail.com>
2024-10-09 00:34:56 +0600

    Fixed session bug

--- app/timetable/page.js
+++ app/timetable/page.js
@@ -2,17 +2,28 @@
 
 import GenerateTimeTable from "@/components/GenerateTimeTable";
 import { UserContext } from "@/contexts/UserContext";
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 const Page = () => {
   const { sessionInfo } = useContext(UserContext);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (sessionInfo && sessionInfo.latest) {
+      setIsLoading(false);
+    }
+  }, [sessionInfo]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="w-full xl:w-3/4 flex flex-col justify-center items-center">
       <p className="font-semibold text-alpha text-lg mb-5 mt-5">
         Showing timetable for current academic session:{" "}
         <span className="underline">
-          {sessionInfo.latest.sesi} - {sessionInfo.latest.semester}
+          {sessionInfo.latest?.sesi} - {sessionInfo.latest?.semester}
         </span>
       </p>
       <GenerateTimeTable />
