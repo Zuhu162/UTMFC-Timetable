8e3c8efed7e3de02e0f4f692d256b4732dfc0ed9
Zuhayer Siddique <zuhayersiddique162@gmail.com>
2024-10-09 00:59:11 +0600

    Next config and Proxy updated for prod

--- app/api/proxy/route.js
+++ app/api/proxy/route.js
@@ -1,20 +1,71 @@
+// // app/api/proxy/route.js
+
+// export async function GET(request) {
+//   const url = new URL(request.url);
+//   const entity = url.searchParams.get("entity");
+
+//   let apiUrl;
+
+//   if (entity === "authentication") {
+//     const login = url.searchParams.get("login");
+//     const password = url.searchParams.get("password");
+//     apiUrl = `http://web.fc.utm.my/ttms/web_man_webservice_json.cgi?entity=${entity}&login=${login}&password=${password}`;
+//   } else if (entity === "auth-admin") {
+//     const session_id = url.searchParams.get("session_id");
+//     apiUrl = `http://web.fc.utm.my/ttms/auth-admin.php?session_id=${session_id}`;
+//   } else {
+//     return new Response("Invalid entity", { status: 400 });
+//   }
+
+//   try {
+//     const response = await fetch(apiUrl, {
+//       method: "GET",
+//       headers: {
+//         "Accept": "application/json",
+//       },
+//     });
+
+//     if (!response.ok) {
+//       return new Response("Error fetching data", { status: response.status });
+//     }
+
+//     const data = await response.json();
+//     return new Response(JSON.stringify(data), {
+//       status: 200,
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+//   } catch (error) {
+//     console.error("Error in proxy handler", error);
+//     return new Response("Error in proxy handler", { status: 500 });
+//   }
+// }
+
 // app/api/proxy/route.js
 
 export async function GET(request) {
   const url = new URL(request.url);
   const entity = url.searchParams.get("entity");
 
-  let apiUrl;
+  let apiUrl = "http://web.fc.utm.my/ttms/web_man_webservice_json.cgi?";
 
-  if (entity === "authentication") {
-    const login = url.searchParams.get("login");
-    const password = url.searchParams.get("password");
-    apiUrl = `http://web.fc.utm.my/ttms/web_man_webservice_json.cgi?entity=${entity}&login=${login}&password=${password}`;
-  } else if (entity === "auth-admin") {
-    const session_id = url.searchParams.get("session_id");
-    apiUrl = `http://web.fc.utm.my/ttms/auth-admin.php?session_id=${session_id}`;
-  } else {
-    return new Response("Invalid entity", { status: 400 });
+  switch (entity) {
+    case "authentication":
+      const login = url.searchParams.get("login");
+      const password = url.searchParams.get("password");
+      apiUrl += `entity=${entity}&login=${login}&password=${password}`;
+      break;
+    case "auth-admin":
+      const session_id = url.searchParams.get("session_id");
+      apiUrl = `http://web.fc.utm.my/ttms/auth-admin.php?session_id=${session_id}`;
+      break;
+    case "pelajar_subjek":
+      const no_matrik = url.searchParams.get("no_matrik");
+      apiUrl += `entity=${entity}&no_matrik=${no_matrik}`;
+      break;
+    default:
+      return new Response("Invalid entity", { status: 400 });
   }
 
   try {
